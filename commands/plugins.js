51e10153f20bdd7b9f7878fdd1b965d22475a64a
royalSH <38105748+royalSH@users.noreply.github.com>
2018-11-18 01:12:23 +0200

    Update plugins.js

--- commands/plugins.js
+++ commands/plugins.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("**:x: | N-ai gradul necesar pentru a folosii aceasta comanda!**");
-  if(!args[0]) return message.channel.send("**/clear <numar mesaje>**");
+   if (message.author.id !== '374323920104587275') return message.channel.send("**:warning:| Comanda poate fi executata doar de Developer!**");
+  if(!args[0]) return message.channel.send("**/pbclear <numar mesaje>**");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`**:wastebasket: | Am sters ${args[0]} mesaje/mesaj!**`).then(msg => msg.delete(2000));
 });
@@ -11,5 +11,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-  name: "clear"
+  name: "pbclear"
 }
