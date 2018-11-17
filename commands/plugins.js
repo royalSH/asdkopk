839c267ecf91fafe9b0557501d6684dd2665c380
royalSH <38105748+royalSH@users.noreply.github.com>
2018-11-18 01:14:46 +0200

    Update plugins.js

--- commands/plugins.js
+++ commands/plugins.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-   if (message.author.id !== '374323920104587275') return message.channel.send("**:warning:| Comanda poate fi executata doar de Developer!**");
+   if (message.author.id !== '337854135976198144') return message.channel.send("**:warning:| Comanda poate fi executata doar de Developer!**");
   if(!args[0]) return message.channel.send("**/pbclear <numar mesaje>**");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`**:wastebasket: | Am sters ${args[0]} mesaje/mesaj!**`).then(msg => msg.delete(2000));
